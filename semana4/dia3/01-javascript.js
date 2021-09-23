c284a8728a26ec94854e2b5aaf57bf84ad9e0363
justincanales <justin_jeriko@hotmail.com>
2021-09-23 18:39:23 -0500

    clases

---
+++ semana4/dia3/01-javascript.js
@@ -0,0 +1,69 @@
+// console.log permite mostrar en la consola de JS, lo que se nos antoje es muy útil para saber que esta pasando internamente dentro de JS
+console.log("Hola Mundo!");
+
+// Variables
+// Palabras reservadas, JS las usa
+//los nombres de variables son sensibles a Mayus y Minus
+var nombre = "justin";
+
+console.log(nombre); //Solamente para mostrar algo
+
+//Tipos de gatos -Texto, Números, Booleanos,
+
+//String - Texto - Cadena de Texto
+var nombre2 = "jeriko";
+
+//Number - número
+var edad = 19;
+
+console.log(edad);
+
+//Boolean - Booleano
+var esDeNoche = true; //true ó false
+console.log(esDeNoche);
+
+// para ver el tipo de dato
+console.log(typeof esDeNoche);
+
+//para cambiar de valor a una variable, me basta con llamarla, esta variable tiene que existir antes
+edad = 20;
+
+console.log(edad);
+
+nombre = "Eduardo";
+
+console.log(nombre);
+
+//Cambiar de tipo de dato
+
+var anio = 2021;
+
+anio = "dos mil veintidos";
+
+console.log(anio);
+
+//JS es un lenguage que no es tipado, no pre declaramos los tipos de datos
+
+//VALORES RAROS EN VARIABLES
+
+var nota; //undefined, no esta definido
+
+console.log(nota);
+
+var nulo = null; //deberia haber algo, pero no esta
+
+console.log(nulo);
+
+// Not a Number = NaN
+var rareza = 100 / "cebollas";
+
+console.log(rareza);
+
+//Infinito
+var infinito = 100 / 0;
+
+infinito = null;
+
+console.log(infinito);
+
+console.log(typeof infinito);
---
+++ semana5/dia1/01-concatenacion.js
@@ -0,0 +1,22 @@
+let a = 1;
+
+let b = "4";
+
+let c = a + b;
+
+console.log(typeof c);
+
+let nombre = "justin";
+
+let apellido = "canales";
+
+let nombrecompleto = nombre + " " + apellido;
+
+console.log(nombrecompleto);
+
+// TEMPLATE STRING / TEMPLATE LITERAL
+
+let texto = `Hola soy ${nombre} ${apellido} y soy de Lima`
+
+console.log(texto)
+
